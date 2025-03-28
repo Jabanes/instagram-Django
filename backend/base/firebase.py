a09960940e198b29a750e5e9030db1fb542d81d0
Your NameJabanes <erezhabani2003@gmail.com>
2025-03-29 00:33:31 +0300

    added firebase to gitignore

--- backend/base/firebase.py
+++ backend/base/firebase.py
@@ -1,7 +1,17 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+import os
+from dotenv import load_dotenv
+import firebase_admin
+from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("base/firebase-credentials.json")
+# Load environment variables from .env file
+load_dotenv()
+
+firebase_credentials_path = os.getenv("FIREBASE_CREDENTIALS_PATH")
+
+cred = credentials.Certificate(firebase_credentials_path)
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
+print("Firebase initialized successfully!")
